d0b527f3e7b30c0d1b527631760ce2ec68f9e3ed
ismailelghazi <ismail47el@gmail.com>
2022-03-26 01:21:52 +0100

    tp12

--- src/main/java/cigma/pfe/repositories/ClientRepository.java
+++ src/main/java/cigma/pfe/repositories/ClientRepository.java
@@ -1,8 +1,8 @@
 package cigma.pfe.repositories;
-
-
 import cigma.pfe.models.Client;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;//crud+pag+jpa
+import org.springframework.data.repository.PagingAndSortingRepository;//crud+pag
+import org.springframework.data.repository.CrudRepository;//crud
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
